772e30c01ee0cbba17472d7fd760a33da24d176d
yogi7302 <mpyogi2226@gmail.com>
2025-11-25 21:04:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 export default function App() {
-  // ------------------ Artwork Data ------------------
+  // ------------------ My Artwork Data ------------------
   const initialArtworks = [
     {
       id: "a1",
