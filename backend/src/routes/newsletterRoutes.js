96d2a3e132fd0eadcfda77b16e1a44070aaaaf74
yogi7302 <mpyogi2226@gmail.com>
2025-12-04 21:01:25 +0530

    Create newsletterRoutes.js

---
+++ backend/src/routes/newsletterRoutes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { subscribeNewsletter } from "../controllers/newsletterController.js";
+
+const router = express.Router();
+router.post("/", subscribeNewsletter);
+
+export default router;
