7a74f718833fe7e11471c5d778b3aa19ab1e0cac
yogi7302 <mpyogi2226@gmail.com>
2025-12-04 20:54:39 +0530

    Create Contact.js

---
+++ backend/src/models/Contact.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Contact", contactSchema);
