82018dd5a8a5b2bea57ef231d532d9585f718bde
yogi7302 <mpyogi2226@gmail.com>
2025-12-04 20:55:16 +0530

    Create Newsletter.js

---
+++ backend/src/models/Newsletter.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const newsletterSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Newsletter", newsletterSchema);
